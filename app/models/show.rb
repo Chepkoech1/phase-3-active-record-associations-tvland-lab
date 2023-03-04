7090dde4177da017651e6781f6acf6d452bf6867
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-03-04 13:01:00 +0300

    done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
   
-end
+    def actors_list
+      self.actors.map(&:full_name)
+    end
+  
+    def show_characters
+      Character.where(show_id: self.id)
+    end
+  end
+  
